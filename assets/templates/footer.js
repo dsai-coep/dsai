8408ca1d1600ebba23068eabd1e7d6565b2866f8
curiyash <curiyash19@gmail.com>
2022-09-11 18:37:27 +0530

    Update team page for responsiveness

--- assets/templates/footer.js
+++ assets/templates/footer.js
@@ -9,7 +9,7 @@ class Footer extends HTMLElement {
       this.innerHTML = `
       <footer class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img class="navbar-img" src="./logos/CoEP.jpeg"></img><img class="navbar-img" src="./logos/DSAI.jpeg"></img></a>
+        <a class="navbar-brand" href="#"><img class="navbar-img" src="./logos/CoEP.png"></img><img class="navbar-img" src="./logos/DSAI.jpeg"></img></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#footerSupportedContent" aria-controls="footerSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -22,7 +22,7 @@ class Footer extends HTMLElement {
               <a class="nav-link" href="https://www.facebook.com/dsaicoep/"><img class="footer-img" src="images/facebook.png"></img></a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="https://www.instagram.com/dsaicoep/"><img class="footer-img" src="images/instagram.png"></img></a>
+               <a class="nav-link" href="https://www.instagram.com/dsai_coep/"><img class="footer-img" src="images/instagram.png"></img></a>
              </li>
           </ul>
         </div>
